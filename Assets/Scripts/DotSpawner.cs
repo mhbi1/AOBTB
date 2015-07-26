5ae195c43d4fd643d52006e07d43ff7965c83181
Michael Bi <michaelhanbi@gmail.com>
2015-07-25 17:24:22 -0700

    Added script to spawn the dots

---
+++ Assets/Scripts/DotSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotSpawner : MonoBehaviour {
+
+	public GameObject[] obj;
+
+	// Use this for initialization
+	void Start () {
+		//start spawning dots
+		Spawn ();
+	}
+
+	void SpawnDot() {
+		//get random coordinates
+		var dotX = Random.Range (0.0f, 5.0f);
+		var dotY = Random.Range (0.0f, 5.0f);
+	}
+}
